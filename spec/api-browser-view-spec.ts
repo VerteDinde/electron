58d43078e1ea90d5a7a765bcf3f9215eedebb163
VerteDinde <vertedinde@electronjs.org>
2022-08-17 07:58:00 -0700

    build: clean up, add GH Action

--- spec/api-browser-view-spec.ts
+++ spec/api-browser-view-spec.ts
@@ -63,7 +63,7 @@ describe('BrowserView module', () => {
     });
 
     // Linux and arm64 platforms (WOA and macOS) do not return any capture sources
-    ifit(process.platform !== 'linux' && process.arch !== 'arm64')('sets the background color to transparent if none is set', async () => {
+    ifit(process.platform !== 'win32' && process.arch == 'x64')('sets the background color to transparent if none is set', async () => {
       const display = screen.getPrimaryDisplay();
       const WINDOW_BACKGROUND_COLOR = '#55ccbb';
 
@@ -82,17 +82,12 @@ describe('BrowserView module', () => {
         x: display.size.width / 2,
         y: display.size.height / 2
       });
-      console.log('*********************');
-      console.log('CENTER COLOR: ', centerColor);
-      console.log('WINDOW_BACKGROUND_COLOR: ', WINDOW_BACKGROUND_COLOR);
-      console.log('*********************');
 
-      expect(areColorsSimilar(centerColor, WINDOW_BACKGROUND_COLOR)).to.be.false();
-      // expect(areColorsSimilar(centerColor, WINDOW_BACKGROUND_COLOR)).to.be.true();
+      expect(areColorsSimilar(centerColor, WINDOW_BACKGROUND_COLOR)).to.be.true();
     });
 
     // Linux and arm64 platforms (WOA and macOS) do not return any capture sources
-    ifit(process.platform !== 'linux' && process.arch !== 'arm64')('successfully applies the background color', async () => {
+    ifit(process.platform == 'darwin' && process.arch == 'x64')('successfully applies the background color', async () => {
       const WINDOW_BACKGROUND_COLOR = '#55ccbb';
       const VIEW_BACKGROUND_COLOR = '#ff00ff';
       const display = screen.getPrimaryDisplay();
